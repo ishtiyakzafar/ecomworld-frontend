1795729705eca058a2198eb3eece72b38d0de880
ishtiyaque.jaffer <ishtiyaque.jaffer@iiflcapital.com>
2025-02-23 16:16:26 +0530

    sdfsf

--- src/components/OnSaleSection/OnSaleSection.jsx
+++ src/components/OnSaleSection/OnSaleSection.jsx
@@ -9,7 +9,7 @@ const OnSaleSection = ({ products }) => {
     <div className={s.onSale}>
       <div className='container'>
         <SectionHeading title='On Sale' />
-        <div className='row g-2 g-md-4'>
+        <div className='row gx-2 gy-3 g-md-4'>
           {
             products.slice(0, 4).map((item) => (
               <div key={item._id} className='col-6 col-md-4 col-lg-3'>
